ef252db0f22567a00591b8ff36a315e9ad582d5f
Md Irfan <iamhero337@gmail.com>
2024-09-13 18:37:36 +0530

    used input for th first time.

---
+++ problems/basic/inputs.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class inputs{
+    public static void main(String args[]) {
+        Scanner sc = new  Scanner(System.in);
+        System.out.print("Give the name: ");
+
+        String name = sc.nextline();
+        
+        System.out.println("The given name is: " + name);
+
+    }
+
+}
