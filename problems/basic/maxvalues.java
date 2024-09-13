f7c47a87e83e28a604cbc91b380dc6087d557f1a
Md Irfan <iamhero337@gmail.com>
2024-09-13 19:13:27 +0530

    every data type max value.

---
+++ problems/basic/maxvalues.java
@@ -0,0 +1,27 @@
+public class doublemaxvalue{
+    public static void main(String args[]) {
+       // Accessing the maximum value for byte
+        Byte maxByte = Byte.MAX_VALUE;
+        System.out.println("The max of Byte data type: " + maxByte);
+
+        // Accessing the maximum value for short
+        Short maxShort = Short.MAX_VALUE;
+        System.out.println("The max of Short data type: " + maxShort);
+
+        // Accessing the maximum value for int
+        Integer maxInt = Integer.MAX_VALUE;
+        System.out.println("The max of Integer data type: " + maxInt);
+
+        // Accessing the maximum value for long
+        Long maxLong = Long.MAX_VALUE;
+        System.out.println("The max of Long data type: " + maxLong);
+
+        // Accessing the maximum value for float
+        Float maxFloat = Float.MAX_VALUE;
+        System.out.println("The max of Float data type: " + maxFloat);
+
+        // Accessing the maximum value for double
+        Double maxDouble = Double.MAX_VALUE;
+        System.out.println("The max of Double data type: " + maxDouble);
+    }
+}
