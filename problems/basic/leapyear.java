8d02c73b47ad92628573418e387afc354ff183af
Md Irfan <iamhero337@gmail.com>
2024-09-24 12:44:50 +0530

    perfect leap year checker with best logic.

--- problems/basic/leapyear.java
+++ problems/basic/leapyear.java
@@ -1,26 +1,17 @@
 import java.util.*;
 
-public class leapyear {
-    public static void main(String args[]) {
+public class LeapYear {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        
         System.out.print("Give the year: ");
         int year = sc.nextInt();
 
-        boolean a = year % 4 == 0;
-        boolean b = year % 100 != 0;
-        boolean c = year % 400 == 0;
-
-
-        if(a && (b || c)) {
+        // Check leap year conditions
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
             System.out.println(year + " is a leap year.");
-        }
-        else {
+        } else {
             System.out.println(year + " is not a leap year.");
         }
-
-         
-
     }
 }
