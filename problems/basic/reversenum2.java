d176e39f2bb894e55ab4c577a35faf08d38f8e96
Md Irfan <iamhero337@gmail.com>
2024-10-16 20:10:26 +0530

    learned for loop

--- problems/basic/reversenum2.java
+++ problems/basic/reversenum2.java
@@ -8,11 +8,16 @@ public class reversenum2 {
         int num = sc.nextInt();
 
         int rev = 0;
-        int i = 0;
-        while(num > 0) {
+//        int i = 0;
+//        while(num > 0) {
+//            i = num % 10;
+//            rev = (rev*10) + i;
+//            num = num/10;
+//        }
+
+        for(int i; num > 0; num= num/10) {
             i = num % 10;
             rev = (rev*10) + i;
-            num = num/10;
         }
 
         System.out.println(rev);
